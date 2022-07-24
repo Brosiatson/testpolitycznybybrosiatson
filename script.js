8c8e81b5871f0c6ba38881082c7865f2ba2b8e70
Brosiatson <brosiatson@gmail.com>
2022-07-24 10:08:25 +0200

    3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const BUTTONS = document.querySelectorAll("button");
-const QUESTIONS_TABLE = ["Jebać gwoździa?", "Jebać PiS", "Jebać wilczka"];
+const QUESTIONS_TABLE = ["Jebać gwoździa", "Jebać PiS", "Podatki to kradzież", "Jebac czarnuhuf", "Monarchia > Anarchia", "Państwo powinno mieć wojsko", "Narodowcy > LGBT"];
 const MAIN = document.querySelector(".main__questions");
 const MAIN_SUBMIT = document.querySelector(".main__submit");
 const SPAN_X = document.querySelector(".span__axis--x");
@@ -12,18 +12,26 @@ let axisY = 0;
 
 const QUESTIONSWITCH = () => {
     BUTTONS[0].addEventListener("click", () => {
-        axisX += 25;
-        axisY += 25;
+        if (questionIndex <= 2) {
+            axisX += 25;
+        }
+        else {
+            axisY += 25;
+        }
     })
     BUTTONS[2].addEventListener("click", () => {
-        axisX -= 25;
-        axisY -= 25;
+        if (questionIndex <= 2) {
+            axisX -= 25;
+        }
+        else {
+            axisY -= 25;
+        }
     })
     BUTTONS.forEach((button) => {
         button.addEventListener("click", () => {
             questionIndex ++;
             document.querySelector("span").innerText = QUESTIONS_TABLE[questionIndex];
-            if (questionIndex === 3) {
+            if (questionIndex === 7) {
                 MAIN.classList.add("off");
                 MAIN_SUBMIT.classList.remove("off");
                 SPAN_X.innerText = axisX;
